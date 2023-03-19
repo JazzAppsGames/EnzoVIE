4c1a2e737dbf10734ce5bef9ff9df8e8cc3408af
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2023-03-19 12:46:26 -0400

    skeletoned element and elementSO

---
+++ Assets/_JazzApps/_Core/Element/Element.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using JazzApps;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace JazzApps
+{
+    public class Element : GameMonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/_JazzApps/_Core/_SOScripts/ElementSO.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using JazzApps;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace JazzApps
+{
+    [CreateAssetMenu(fileName = "Element", menuName = "SOs/Element", order = 1)]
+    public class ElementSO : GameScriptableObject
+    {
+        
+    }
+}
